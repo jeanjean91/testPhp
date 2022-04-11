9248d34610b556ac5e27b5e3b34fed19d2ad3343
jeanjean91 <36715623+jeanjean91@users.noreply.github.com>
2022-04-12 01:19:45 +0200

    config

--- Models/database.php
+++ Models/database.php
@@ -1,13 +1,21 @@
 
 <?php  
-try 
-{
-    $dbcon=mysqli_connect("localhost","root","root");  
-    mysqli_select_db($dbcon,"myPhpTest");               
-} 
-catch (Exception $ex) 
-{
-    echo "echec connection ";
+// try 
+// {
+//     $dbcon=mysqli_connect("localhost","root","root");  
+//     mysqli_select_db($dbcon,"myPhpTest");               
+// } 
+// catch (Exception $ex) 
+// {
+//     echo "echec connection ";
+// }
+
+$dbcon = new mysqli("localhost","root","root","myPhpTest");
+
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
 }
 ?>  
 
--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 bonjour, voici le test. 
 J'ai ue quelques petits problemes lorsque je veux include la bdd 
 
-ducoup je l'ai mis en ds le model.. 
+ducoup je l'ai mis directement dans les fichier model.. 
 
 Vous pouvez utiliser les username existant ou créer le votre.
 
+config mac: $dbcon = mysqli_connect('localhost', 'root', 'root', 'myPhpTest');
+config windows  $dbcon = mysqli_connect('localhost', 'root', '', 'myPhpTest');
